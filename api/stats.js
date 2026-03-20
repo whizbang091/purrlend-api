6a41c334ee4c8abf19d9850bb6357994166260ba
whizbang091 <whizbanguiux@gmail.com>
2026-03-20 11:24:28 +0100

    Update stats.js

--- api/stats.js
+++ api/stats.js
@@ -7,8 +7,7 @@ const UI_POOL_DATA_PROVIDER = "0x0C591b5A3615c21cbd09F028F2E4509C2938F65E";
 const ABI = ["function getReservesData(address provider) view returns ((address underlyingAsset, string name, string symbol, uint256 decimals, uint256 baseLTVasCollateral, uint256 reserveLiquidationThreshold, uint256 reserveLiquidationBonus, uint256 reserveFactor, bool usageAsCollateralEnabled, bool borrowingEnabled, bool isActive, bool isFrozen, uint128 liquidityIndex, uint128 variableBorrowIndex, uint128 liquidityRate, uint128 variableBorrowRate, uint40 lastUpdateTimestamp, address aTokenAddress, address variableDebtTokenAddress, address interestRateStrategyAddress, uint256 availableLiquidity, uint256 totalVariableDebt, uint256 priceInMarketReferenceCurrency, uint256 variableRateSlope1, uint256 variableRateSlope2, uint256 baseVariableBorrowRate, uint256 optimalUsageRatio, uint256 totalAToken, uint256 priceInEth, uint256 accruedToTreasury, uint256 unbacked, uint256 isolationModeTotalDebt, bool flashLoanEnabled, uint256 debtCeiling, uint256 debtCeilingDecimals, uint8 eModeCategoryId, uint256 borrowCap, uint256 supplyCap, uint16 eModeLtv, uint16 eModeLiquidationThreshold, uint16 eModeLiquidationBonus, address eModePriceSource, string eModeLabel, bool borrowableInIsolation)[], (uint256 marketReferenceCurrencyUnit, int256 marketReferencePriceInUsd, uint256 networkBaseTokenPriceInUsd))"];
 
 function formatUSD(dollars) {
-  const str = dollars.toString();
-  return "$" + str.replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ".00";
+  return "$" + dollars.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ".00";
 }
 
 export default async function handler(req, res) {
@@ -27,8 +26,9 @@ export default async function handler(req, res) {
       const decimals = BigInt(r.decimals.toString());
       const price    = BigInt(r.priceInMarketReferenceCurrency.toString());
       const amount   = BigInt(r.totalAToken.toString());
-      // price is always in 1e43 units regardless of token decimals
-      totalMarketSize += amount * price / 10n ** decimals / 10n ** 43n;
+      // normalize all tokens to 18 decimals first, then apply fixed 43 divisor
+      const normalized = amount * 10n ** (18n - decimals);
+      totalMarketSize += normalized * price / 10n ** 18n / 10n ** 43n;
     }
 
     return res.status(200).json({
